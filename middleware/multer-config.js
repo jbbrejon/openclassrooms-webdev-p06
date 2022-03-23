42e5ddbd58554d61ece9d25ceee3848f3cca389b
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-03-23 09:56:32 +0100

    Configure 'Multer' middlware (file management: naming, add, remove)

---
+++ middleware/multer-config.js
@@ -0,0 +1,29 @@
+// SCRIPT DESCRIPTION: Middelware for "Multer" configuration (file management : naming, add, remove)
+
+
+// Module dependencies
+const multer = require('multer'); // https://www.npmjs.com/package/multer
+
+// Set extension for file name
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpeg',
+    'image/png': 'png'
+};
+
+// Call diskStorage method from multer module
+const storage = multer.diskStorage({
+    // Set destination
+    destination: (req, file, callback) => {
+        callback(null, 'images')
+    },
+    // Set filename
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_'); // replace " " with "_" in filename (if applicable)
+        const extension = MIME_TYPES[file.mimetype]; // extension corresponding to the MIME type of the file in the POST request
+        callback(null, name + Date.now() + '.' + extension); // add time stamp to file
+    }
+});
+
+// Make module available through require() from other project scripts (https://nodejs.org/api/modules.html#module)
+module.exports = multer({ storage }).single('image'); // Call single() method from Multer module (specify it's an operation on a single file, not a bulk operation)
