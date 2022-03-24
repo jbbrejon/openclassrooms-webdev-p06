adcd276589ca8173151961c10ac57e6d1dbe85eb
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-03-24 15:13:32 +0100

    update comments

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,11 @@
-
 // SCRIPT DESCRIPTION: Middleware for authentication with token (used to secure sensitive routes in ../routes/sauce)
 
 // Module dependencies
 const jwt = require('jsonwebtoken'); // https://www.npmjs.com/package/jsonwebtoken
 const env = require('dotenv').config(); // https://www.npmjs.com/package/dotenv
 
-// Set variable : token string for 'sign()' method of jsonwebtoken module
-const tokenString = process.env.JSON_WEB_TOKEN; // Token string to be set in ./.env (not synced with git)
+// Set variable : token string for 'sign()' method of jsonwebtoken module (JSON_WEB_TOKEN to  set in ./.env)
+const tokenString = process.env.JSON_WEB_TOKEN;
 
 // Make module available through require() from other project scripts (https://nodejs.org/api/modules.html#module)
 module.exports = (req, res, next) => {
@@ -22,6 +21,6 @@ module.exports = (req, res, next) => {
         }
 
     } catch (error) {
-        res.status(401).json({ error: error | 'Request not authenticated!' }); // If error -> return status code 201 with error message  (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401)
+        res.status(401).json({ error: error | 'Request not authenticated!' }); // If error -> return status code 401 with error message  (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401)
     }
 };
