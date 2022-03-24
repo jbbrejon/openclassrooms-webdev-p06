2bf13c5e506989c1482b2e33b0b064c65d4bf721
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-03-24 12:55:07 +0100

    add 'email-validator', 'password-validator' and 'rate-limit' middlewares to user routes

--- routes/user.js
+++ routes/user.js
@@ -4,15 +4,18 @@
 // Module dependencies
 const express = require('express'); // (https://www.npmjs.com/package/express)
 const userCtrl = require('../controllers/user'); // local module
+const emailValidator = require('../middleware/email-validator') // local module (middleware for email validation)
+const passwordValidator = require('../middleware/password-validator') // local module (middleware for password validation)
+const rateLimit = require('../middleware/rate-limit')
 
 // Create "Express" router with "Router()" method (http://expressjs.com/en/5x/api.html#router)
 const router = express.Router();
 
 // Set route : "/api/auth/signup" POST requests (arguments : request -> address, response -> from controllers/user)
-router.post('/signup', userCtrl.signup);
+router.post('/signup', emailValidator, passwordValidator, userCtrl.signup);
 
 // Set route : "/api/auth/login" POST requests (arguments : request -> address, response -> from controllers/user)
-router.post('/login', userCtrl.login);
+router.post('/login', rateLimit, userCtrl.login);
 
 // Make module available through require() from other project scripts (https://nodejs.org/api/modules.html#module)
 module.exports = router;
