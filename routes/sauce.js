665e75acc33552cd1eb6e483d8223d39e25c0fa4
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-03-24 15:08:31 +0100

    update comments

--- routes/sauce.js
+++ routes/sauce.js
@@ -6,22 +6,21 @@ const sauceCtrl = require('../controllers/sauce'); // local module ("sauce" cont
 const auth = require('../middleware/auth'); // local module ("auth" middleware)
 const multer = require('../middleware/multer-config'); // local module ("multer-config" middleware)
 
-
 // Create "Express" router with "Router()" method (http://expressjs.com/en/5x/api.html#router)
 const router = express.Router();
 
-
-// Set route : "/api/sauces" GET request (arguments : request -> address, middleware -> auth, response -> from controllers/sauce)
+// Set route : "/api/sauces" GET requests
 router.get('/', auth, sauceCtrl.getAllSauces);
-// Set (dynamic) route : "/api/sauces/:id" GET request (arguments : request -> address, middleware -> auth, response -> from controllers/sauce)
+// Set (dynamic) route : "/api/sauces/:id" GET requests
 router.get('/:id', auth, sauceCtrl.getOneSauce);
-// Set route : "/api/sauces" POST request (arguments : request -> address, middleware -> auth, middleware -> multer, response -> from controllers/sauce)
+// Set route : "/api/sauces" POST requests
 router.post('/', auth, multer, sauceCtrl.createSauce);
-// Set (dynamic) route : "/api/sauces/:id/like" POST request (arguments : request -> address, middleware -> auth, response -> from controllers/sauce)
+// Set (dynamic) route : "/api/sauces/:id/like" POST requests
 router.post('/:id/like', auth, sauceCtrl.likeSauce);
-// Set (dynamic) route : "/api/sauces/:id" PUT request (arguments : request -> address, middleware -> auth, middleware -> multer, response -> from controllers/sauce)
+// Set (dynamic) route : "/api/sauces/:id" PUT requests
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-// Set (dynamic) route : "/api/sauces/:id" DELETE request (arguments : request -> address, middleware -> auth, response -> from controllers/sauce)
+// Set (dynamic) route : "/api/sauces/:id" DELETE requests
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
+// Make module available through require() from other project scripts (https://nodejs.org/api/modules.html#module)
 module.exports = router;
