2b596e6e37bde7d8f36fcbcf41e585502c9190f9
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-03-24 12:54:00 +0100

    configure middleware to limit connection attempts

---
+++ middleware/rate-limit.js
@@ -0,0 +1,15 @@
+// SCRIPT DESCRIPTION : set connection attempts to 5 and block new attemps for 10 minutes
+
+// Module dependencies
+const rateLimit = require('express-rate-limit'); // https://www.npmjs.com/package/express-rate-limit
+
+// Set limit and 
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 5,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: "5 unsuccessfull connections. You're blocked for 10 minutes!"
+});
+
+module.exports = limiter;
