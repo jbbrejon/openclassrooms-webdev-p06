c367ab11dc4d671623d6656233c600eededc8c4f
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-03-24 15:21:10 +0100

    update comments

--- middleware/rate-limit.js
+++ middleware/rate-limit.js
@@ -1,9 +1,9 @@
-// SCRIPT DESCRIPTION : set connection attempts to 5 and block new attemps for 10 minutes
+// SCRIPT DESCRIPTION : set connection attempts to 5 and block new attemps for 10 minutes (called from ../routes/user)
 
 // Module dependencies
 const rateLimit = require('express-rate-limit'); // https://www.npmjs.com/package/express-rate-limit
 
-// Set limit and 
+// Set limit (5 attempts max, then blocked for 10 minutes) 
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000,
     max: 5,
